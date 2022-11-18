018ddde5d02d8439f25a9c41cb57e4b5cd975a4b
jingyu9804 <swc05161@gmail.com>
2022-11-18 23:01:34 +0900

    feat(dfs) : 1890 문제 해결

--- src/main/java/cote/baekjoon/dp/NO1890.java
+++ src/main/java/cote/baekjoon/dp/NO1890.java
@@ -5,9 +5,17 @@ import java.util.Scanner;
 public class NO1890 {
     private static int n;
     private static int[][] map;
+    private static long[][] dp;
 
     public static void solution(){
-
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int next = map[i][j];
+                if(next==0) break;
+                if(i+next<n) dp[i+next][j] += dp[i][j];
+                if(j+next<n) dp[i][j+next] += dp[i][j];
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -20,8 +28,11 @@ public class NO1890 {
                 map[i][j] = sc.nextInt();
             }
         }
+        dp = new long[n][n];
+        dp[0][0] = 1;
+        solution();
 
-
+        System.out.println(dp[n-1][n-1]);
     }
 }
 
