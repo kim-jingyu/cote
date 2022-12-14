09b912dc32042d5bee74c1df7525a1d6a7315944
jingyu9804 <swc05161@gmail.com>
2022-12-14 16:38:42 +0900

    feat(programmers) : GameMapMinLength 프로덕트 코드 구현 (3차시도 - bfs) (visited 배열 선언-성공!)

--- src/main/java/cote/programmers/GameMapMinLength.java
+++ src/main/java/cote/programmers/GameMapMinLength.java
@@ -16,38 +16,39 @@ public class GameMapMinLength {
     int[] dy = {1,0,-1,0};
     int[] dx = {0,1,0,-1};
     private int[][] maps, dis;
+    private boolean[][] visited;
 
     private void bfs(int cur_y,int cur_x) {
         Queue<Point> queue = new LinkedList<>();
         queue.offer(new Point(cur_y, cur_x));
         dis[cur_y][cur_x] = 1;
+        visited[cur_y][cur_x] = true;
         while (!queue.isEmpty()) {
             Point point = queue.poll();
+
+            if(dis[maps.length-1][maps[0].length-1]>0) return;
+
             for (int i = 0; i < 4; i++) {
                 int ny = point.y + dy[i];
                 int nx = point.x + dx[i];
 
-                if(ny < 0 || ny > maps.length-1 || nx < 0 || nx > maps[0].length-1){
+                if(ny < 0 || ny > maps.length-1 || nx < 0 || nx > maps[0].length-1 || maps[ny][nx] == 0 || visited[ny][nx]){
                     continue;
                 }
 
-                if(maps[ny][nx] == 1){
-                    maps[point.y][point.x] = 0;
-                    queue.offer(new Point(ny, nx));
-                    dis[ny][nx] = dis[point.y][point.x] + 1;
-                }
+                maps[point.y][point.x] = 0;
+                visited[ny][nx] = true;
+                queue.offer(new Point(ny, nx));
+                dis[ny][nx] = dis[point.y][point.x] + 1;
             }
         }
     }
     public int solution(int[][] maps){
         this.maps = maps;
         dis = new int[maps.length][maps[0].length];
+        visited = new boolean[maps.length][maps[0].length];
         bfs(0,0);
         if(dis[maps.length-1][maps[0].length-1]>0) return dis[maps.length-1][maps[0].length-1];
         return -1;
     }
 }
-
-//    maps	answer
-//[[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]	11
-//        [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]	-1
