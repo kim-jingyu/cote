8718749b13d7aa0758a7e98d37e7962047274828
jingyu9804 <swc05161@gmail.com>
2022-12-14 17:08:35 +0900

    feat(programmers) : Network 프로덕션 코드 생성

---
+++ src/main/java/cote/programmers/Network.java
@@ -0,0 +1,14 @@
+package cote.programmers;
+
+class Network {
+    private
+
+    public int solution(int n, int[][] computers){
+
+    }
+}
+
+//| n | computers | return |
+//        | --- | --- | --- |
+//        | 3 | [[1, 1, 0], [1, 1, 0], [0, 0, 1]] | 2 |
+//        | 3 | [[1, 1, 0], [1, 1, 1], [0, 1, 1]] | 1 |
