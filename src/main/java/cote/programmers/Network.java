53a6af8cd18632dc5c305edc889c9984436cd1fe
jingyu9804 <swc05161@gmail.com>
2022-12-15 20:05:27 +0900

    feat(programmers) : Network 프로덕션 코드 dfs로 구현

--- src/main/java/cote/programmers/Network.java
+++ src/main/java/cote/programmers/Network.java
@@ -1,10 +1,37 @@
 package cote.programmers;
 
 class Network {
-    private
+    private int n;
+    private int[][] computers;
+    private boolean[] visited;
+
+    private void init(int n, int[][] computers) {
+        this.n = n;
+        this.computers = computers;
+    }
+
+    private void dfs(int cur) {
+        visited[cur] = true;
+        for (int next = 0; next < n; next++) {
+            if(cur!=next && computers[cur][next]==1 && !visited[next]){
+                dfs(next);
+            }
+        }
+    }
 
     public int solution(int n, int[][] computers){
+        init(n, computers);
+        int answer = 0;
+        visited = new boolean[n];
 
+        for (int i = 0; i < n; i++) {
+            if(!visited[i]){
+                dfs(i);
+                answer++;
+            }
+        }
+
+        return answer;
     }
 }
 
