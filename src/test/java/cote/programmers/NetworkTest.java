59dcf0a5a98871065919ead2a4d83abaeb5d9f58
jingyu9804 <swc05161@gmail.com>
2022-12-15 20:05:09 +0900

    style(programmers) : Network 테스트 코드 수정

--- src/test/java/cote/programmers/NetworkTest.java
+++ src/test/java/cote/programmers/NetworkTest.java
@@ -2,7 +2,6 @@ package cote.programmers;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 class NetworkTest {
