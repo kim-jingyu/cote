ba0cea0bd934d1c1daf8f8c1d6b10ecf075794bb
jingyu9804 <swc05161@gmail.com>
2023-04-16 01:33:59 +0900

    test(programmers) : 2023 KAKAO BLIND RECRUITMENT - 택배 배달과 수거하기

--- src/test/java/cote/programmers/DeliveryAndPickupsTest.java
+++ src/test/java/cote/programmers/DeliveryAndPickupsTest.java
@@ -3,6 +3,7 @@ package cote.programmers;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeliveryAndPickupsTest {
@@ -12,13 +13,13 @@ class DeliveryAndPickupsTest {
     void testcase1() {
         long result = deliveryAndPickups.solution(4, 5, new int[]{1, 0, 3, 1, 2}, new int[]{0, 3, 0, 4, 0});
 
-        Assertions.assertThat(result).isEqualTo(16);
+        assertThat(result).isEqualTo(16);
     }
 
     @Test
     void testcase2() {
         long result = deliveryAndPickups.solution(2, 7, new int[]{1, 0, 2, 0, 1, 0, 2}, new int[]{0, 2, 0, 1, 0, 2, 0});
 
-        Assertions.assertThat(result).isEqualTo(30);
+        assertThat(result).isEqualTo(30);
     }
 }
