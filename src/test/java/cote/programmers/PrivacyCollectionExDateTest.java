e19175ae27d6e2c890634511850e30656bd234a6
jingyu9804 <swc05161@gmail.com>
2023-04-15 22:04:00 +0900

    test(programmers) : 2023 KAKAO BLIND RECRUITMENT - 개인정보 수집 유효기간

--- src/test/java/cote/programmers/PrivacyCollectionExDateTest.java
+++ src/test/java/cote/programmers/PrivacyCollectionExDateTest.java
@@ -12,26 +12,16 @@ class PrivacyCollectionExDateTest {
 
     @Test
     void testcase1() {
-        String today = "2022.05.19";
-        String[] terms = {"A 6", "B 12", "C 3"};
-        String[] privacies = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
-        int[] answer = {1, 3};
+        int[] result = privacyCollectionExDate.solution("2022.05.19", new String[]{"A 6", "B 12", "C 3"}, new String[]{"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"});
 
-        int[] solution = privacyCollectionExDate.solution(today, terms, privacies);
-
-        assertThat(solution).isEqualTo(answer);
+        assertThat(result).isEqualTo(new int[]{1, 3});
     }
 
     @Test
     void testcase2() {
-        String today = "2020.01.01";
-        String[] terms = {"Z 3", "D 5"};
-        String[] privacies = {"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"};
-        int[] answer = {1, 4, 5};
+        int[] result = privacyCollectionExDate.solution("2020.01.01", new String[]{"Z 3", "D 5"}, new String[]{"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"});
 
-        int[] solution = privacyCollectionExDate.solution(today, terms, privacies);
-
-        assertThat(solution).isEqualTo(answer);
+        assertThat(result).isEqualTo(new int[]{1, 4, 5});
     }
 
 }
