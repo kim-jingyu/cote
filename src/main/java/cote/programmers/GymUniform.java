46c20f1efa0355a2747d0921e3d578b591013a9c
jingyu9804 <swc05161@gmail.com>
2023-04-16 23:08:27 +0900

    feat(programmers) : Greedy - 체육복

--- src/main/java/cote/programmers/GymUniform.java
+++ src/main/java/cote/programmers/GymUniform.java
@@ -1,46 +1,17 @@
 package cote.programmers;
 
+import java.util.Arrays;
+
 public class GymUniform {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        int[] curLost = new int[n];
-        int[] curReserve = new int[n];
+        int[] students = new int[n + 1];
+        Arrays.fill(students, 1);
 
-        // 초기화
-        for (int cur : lost) {
-            curLost[cur-1] = 1;
-        }
-        for (int cur : reserve) {
-            curReserve[cur-1] = 1;
-        }
+        for (int curLost : lost) students[curLost]--;
+        for (int curReserve : reserve) students[curReserve]++;
 
-        // lost, reserve 중복 제거
-        for (int i = 0; i < n; i++) {
-            if (curLost[i] == 1 && curReserve[i] == 1) {
-                curLost[i] = 0;
-                curReserve[i] = 0;
-            }
-        }
 
-        for (int i = 0; i < n; i++) {
-            if (curReserve[i] == 1) {
-                if (i - 1 == -1) {
-                    curLost[i + 1] = 0;
-                    continue;
-                }
-                if (i + 1 == n) {
-                    curLost[i - 1] = 0;
-                    continue;
-                }
-                curLost[i - 1] = 0;
-            }
-        }
-
-        for (int cur : curLost) {
-            if (cur == 1) {
-                answer++;
-            }
-        }
 
         return n - answer;
     }
