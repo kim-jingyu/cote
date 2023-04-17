4e5b8f879ccb0266b4bfe3fdb4cc55877350e692
jingyu9804 <swc05161@gmail.com>
2023-04-17 21:41:53 +0900

    feat(programmers) : 탐욕법(Greedy) - 큰 수 만들기

---
+++ src/main/java/cote/programmers/MakeMaxNumber.java
@@ -0,0 +1,13 @@
+package cote.programmers;
+
+public class MakeMaxNumber {
+    public String solution(String number, int k) {
+        String answer = "";
+        String[] split = number.split("");
+
+
+
+
+        return answer;
+    }
+}
