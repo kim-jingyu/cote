df334f274f88bd810cc63ec93e95cc5657519ef3
jingyu9804 <swc05161@gmail.com>
2023-04-23 23:45:29 +0900

    feat(programmers) : 완전탐색 - 피로도

---
+++ src/main/java/cote/programmers/Fatigue.java
@@ -0,0 +1,13 @@
+package cote.programmers;
+
+public class Fatigue {
+    /**
+     * @param k, 현재 피로도
+     * @param dungeons, 각 던전별 "최소 필요 피로도", "소모 피로도"
+     * @return 유저가 탐험할 수 있는 최대 던전 수
+     */
+    public int solution(int k, int[][] dungeons) {
+        int answer = 0;
+        return answer;
+    }
+}
