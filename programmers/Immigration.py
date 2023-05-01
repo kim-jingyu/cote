79123729728d84c677f0573c3b6be2857464ffb4
jingyu9804 <swc05161@gmail.com>
2023-05-02 02:01:53 +0900

    feat(programmers) : 이분탐색 - 입국심사

---
+++ programmers/Immigration.py
@@ -0,0 +1,23 @@
+# n: 사람수, times[] : 심사관별 검사 시간
+def solution(n, times):
+    start = 1
+    end = n*max(times)
+    answer = end
+
+    while (start <= end):
+        mid = (start + end) // 2
+
+        people = 0
+        for time in times:
+            people += (mid // time)
+
+        if people < n:
+            start = mid + 1
+        else:
+            end = mid - 1
+            answer = min(answer, mid)
+
+    return answer
+
+
+solution(6, {7, 10})
