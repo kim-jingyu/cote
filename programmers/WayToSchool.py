b671d9f5e26add775a48bcb51de4eb4d0f8fc692
jingyu9804 <swc05161@gmail.com>
2023-05-04 19:16:43 +0900

    feat(programmers): 등굣길

--- programmers/WayToSchool.py
+++ programmers/WayToSchool.py
@@ -1,13 +1,25 @@
 def solution(m, n, puddles):
     answer = 0
 
-    # map 1로 채우기
-    map = [[1 for j in range(n+1)] for i in range(m+1)]
+    # dp 0으로 채우기
+    dp = [ [0] * (m+1) for i in range(n+1) ]
 
-    # 물웅덩이 0으로 채우기
-    for i, j in puddles:
-        map[i][j] = 0
+    puddles = [ [i,j] for [j,i] in puddles]
 
+    # 점화식: dp[i][j](새로 값을 넣을 칸) = dp[i-1][j](왼쪽 칸까지의 이동수) + dp[i][j-1](윗쪽 칸까지의 이동수)
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            # 집인 경우, 반복문 건너뛰기
+            if (i==1 and j==1):
+                dp[i][j] = 1
+                continue
+            # 물웅덩이인 경우, 0으로 채우기
+            if [i, j] in puddles:
+                dp[i][j] = 0
+                continue
+            dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000007
+
+    answer = dp[n][m]
     return answer
 
 
