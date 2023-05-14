cf82a1383363f115f17242b52ed6230ed04971e5
jingyu9804 <swc05161@gmail.com>
2023-05-14 14:09:23 +0900

    feat(programmers) : DFS - 여행경로

--- src/main/java/cote/programmers/TravelPath.java
+++ src/main/java/cote/programmers/TravelPath.java
@@ -4,40 +4,31 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class TravelPath {
-    List<String> result;
-    String[][] tickets;
-    boolean[] visited;
+class TravelPath {
+    public String[] solution(String[][] tickets) {
+        List<String> result = new ArrayList<>();
+        boolean[] isVisited = new boolean[tickets.length];
 
-    private void init(String[][] tickets) {
-        this.tickets = tickets;
-        visited = new boolean[tickets.length];
-        result = new ArrayList<>();
+        dfs(0, "ICN", "ICN", tickets, isVisited, result);
+
+        Collections.sort(result);
+
+        return result.get(0).split(" ");
     }
 
-    private void dfs(int Level,String departure,String path) {
-        if (Level == tickets.length) {
-            result.add(path);
+    private void dfs(int level, String departure, String path, String[][] tickets, boolean[] isVisited, List<String> answer) {
+        if (level == tickets.length) {
+            answer.add(path);
             return;
         }
         for (int i = 0; i < tickets.length; i++) {
-            if(visited[i]) continue;
+            if (isVisited[i]) continue;
 
-            if (departure.equals(tickets[i][0])) {
-                visited[i] = true;
-                dfs(Level + 1, tickets[i][1], path + " " + tickets[i][1]);
-                visited[i] = false;
+            if (tickets[i][0].equals(departure)) {
+                isVisited[i] = true;
+                dfs(level+1, tickets[i][1], path + " " + tickets[i][1], tickets, isVisited, answer);
+                isVisited[i] = false;
             }
         }
     }
-
-    public String[] solution(String[][] tickets) {
-        init(tickets);
-        dfs(0, "ICN", "ICN");
-        Collections.sort(result);
-        return result.get(0).split(" ");
-    }
 }
-//
-//[["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]	["ICN", "JFK", "HND", "IAD"]
-//        [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]	["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"]
