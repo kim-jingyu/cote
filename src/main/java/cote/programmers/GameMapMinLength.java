d62e013295d7f35566f7eaa9f9c889ad74c3f5ce
jingyu9804 <swc05161@gmail.com>
2023-05-14 23:07:37 +0900

    refactor(programmers) : BFS - 게임 맵 최단거리

--- src/main/java/cote/programmers/GameMapMinLength.java
+++ src/main/java/cote/programmers/GameMapMinLength.java
@@ -3,52 +3,57 @@ package cote.programmers;
 import java.util.LinkedList;
 import java.util.Queue;
 
-class Point{
-    int y;
-    int x;
+class GameMapMinLength {
+    static class Point{
+        int x;
+        int y;
 
-    public Point(int y, int x) {
-        this.y = y;
-        this.x = x;
-    }
-}
-public class GameMapMinLength {
-    int[] dy = {1,0,-1,0};
-    int[] dx = {0,1,0,-1};
-    private int[][] maps, dis;
-    private boolean[][] visited;
-
-    private void bfs(int cur_y,int cur_x) {
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(cur_y, cur_x));
-        dis[cur_y][cur_x] = 1;
-        visited[cur_y][cur_x] = true;
-        while (!queue.isEmpty()) {
-            Point point = queue.poll();
-
-            if(dis[maps.length-1][maps[0].length-1]>0) return;
-
-            for (int i = 0; i < 4; i++) {
-                int ny = point.y + dy[i];
-                int nx = point.x + dx[i];
-
-                if(ny < 0 || ny > maps.length-1 || nx < 0 || nx > maps[0].length-1 || maps[ny][nx] == 0 || visited[ny][nx]){
-                    continue;
-                }
-
-                maps[point.y][point.x] = 0;
-                visited[ny][nx] = true;
-                queue.offer(new Point(ny, nx));
-                dis[ny][nx] = dis[point.y][point.x] + 1;
-            }
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
-    public int solution(int[][] maps){
-        this.maps = maps;
-        dis = new int[maps.length][maps[0].length];
-        visited = new boolean[maps.length][maps[0].length];
-        bfs(0,0);
-        if(dis[maps.length-1][maps[0].length-1]>0) return dis[maps.length-1][maps[0].length-1];
-        return -1;
+
+    public int solution(int[][] maps) {
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+
+        int[][] distance = new int[maps.length][maps[0].length];
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
+
+        return bfs(maps, dx, dy, distance, visited);
+    }
+
+    private int bfs(int[][] maps, int[] dx, int[] dy, int[][] distance, boolean[][] visited) {
+        int result = -1;
+        Point departure = new Point(0, 0);    // 현재 위치
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(departure);
+        distance[0][0] = 1;
+        visited[0][0] = true;
+
+        while (!queue.isEmpty()) {
+            Point cur = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = cur.x + dx[i];
+                int nextY = cur.y + dy[i];
+
+                if (nextX < 0 || nextY < 0 || nextX >= maps[0].length || nextY >= maps.length || visited[nextY][nextX]) continue;
+
+                if (maps[nextY][nextX] == 1) {
+                    maps[cur.y][cur.x] = 0;
+                    visited[nextY][nextX] = true;
+                    distance[nextY][nextX] = distance[cur.y][cur.x] + 1;
+                    queue.add(new Point(nextX, nextY));
+                }
+            }
+        }
+
+        if (distance[maps.length - 1][maps[0].length - 1] != 0) {
+            result = distance[maps.length - 1][maps[0].length - 1];
+        }
+
+        return result;
     }
 }
