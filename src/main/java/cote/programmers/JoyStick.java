c310b8afe28c5fd127d926f24154e0bcebee0256
jingyu9804 <swc05161@gmail.com>
2023-05-16 21:02:22 +0900

    refactor(programmers) : Greedy - 조이스틱

--- src/main/java/cote/programmers/JoyStick.java
+++ src/main/java/cote/programmers/JoyStick.java
@@ -12,17 +12,15 @@ public class JoyStick {
             // 상하 이동거리 구하기
             answer += Math.min(name.charAt(cur) - 'A', 'Z' - name.charAt(cur) + 1);
 
-            // A의 마지막 위치
-            int lastPosA = cur + 1;
-            while (lastPosA < nameLen && name.charAt(lastPosA) == 'A') {
-                lastPosA++;
+            // A를 제외한 다음 index
+            int next = cur + 1;
+            while (next < nameLen && name.charAt(next) == 'A') {
+                next++;
             }
 
             // 좌우 이동거리 구하기
-            // cur = 오른쪽 이동거리
-            // name - lastPosA = 왼쪽 이동거리
-            // Math.min(cur, nameLen - lastPosA) = 끝에서 해당 숫자까지 이동거리, 오른쪽 이동거리 중 작은 값
-            move = Math.min(move, cur + (nameLen - lastPosA) + Math.min(cur, nameLen - lastPosA));
+            int distance = Math.min(cur, nameLen - next);
+            move = Math.min(move, cur + nameLen - next + distance);
         }
 
         return answer + move;
