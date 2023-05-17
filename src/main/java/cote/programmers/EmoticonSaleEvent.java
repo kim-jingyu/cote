d1129ca3662bf635fdf3e7bb06676a62640f2eb6
jingyu9804 <swc05161@gmail.com>
2023-05-17 17:59:29 +0900

    feat(programmers) : 2023 KAKAO BLIND RECRUITMENT - 이모티콘할인행사

---
+++ src/main/java/cote/programmers/EmoticonSaleEvent.java
@@ -0,0 +1,11 @@
+package cote.programmers;
+
+/**
+ * 2023 KAKAO BLIND RECRUITMENT
+ */
+public class EmoticonSaleEvent {
+    public int[] solution(int[][] users, int[] emoticons) {
+        int[] answer = {};
+        return answer;
+    }
+}
