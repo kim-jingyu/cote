1fe71173cd190ddbc07e1e9dee9380cb6e41edf4
jingyu9804 <swc05161@gmail.com>
2023-05-20 15:54:36 +0900

    feat(programmers) : SWEA 1244. 최대 상금

---
+++ src/main/java/cote/swea/MaxPrizeMoney.java
@@ -0,0 +1,62 @@
+package cote.swea;
+
+import java.util.*;
+
+public class MaxPrizeMoney {
+    private static int answer;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int testcases = scanner.nextInt();
+
+        for (int i = 1; i <= testcases; i++) {
+            int number = scanner.nextInt();
+            int exchange = scanner.nextInt();
+            answer = 0;
+
+            dfs(0, String.valueOf(number).split(""), exchange, new boolean[exchange + 1][1000000]);
+
+            System.out.println("#" + i + " " + answer);
+        }
+
+    }
+
+    private static void dfs(int level, String[] numbers, int exchange, boolean[][] isVisited) {
+        if (level == exchange) {
+            String flattenedNumber = getFlattenedNumber(numbers);
+            answer = getMaxAnswer(flattenedNumber);
+            return;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                exchanging(numbers, i, j);
+
+                String flattenedNumber = getFlattenedNumber(numbers);
+
+                if(!isVisited[level][Integer.valueOf(flattenedNumber)]) {
+                    isVisited[level][Integer.valueOf(flattenedNumber)] = true;
+                    dfs(level+1, numbers, exchange, isVisited);
+                }
+
+                exchanging(numbers, i, j);
+            }
+        }
+    }
+
+    private static String getFlattenedNumber(String[] numbers) {
+        String flattenedNumber = "";
+        for (String number : numbers) {
+            flattenedNumber += number;
+        }
+        return flattenedNumber;
+    }
+
+    private static void exchanging(String[] numbers, int i, int j) {
+        String tmp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = tmp;
+    }
+
+    private static int getMaxAnswer(String flattenedNumber) {
+        return Math.max(answer, Integer.valueOf(flattenedNumber.toString()));
+    }
+}
