a8a2f1ad269f316f340d891b341b8cd8107f0991
jingyu9804 <swc05161@gmail.com>
2023-05-26 09:28:28 +0900

    feat(programmers) : 연속된 부분 수열의 합

---
+++ src/test/java/cote/programmers/SumOfContinuedPartialSequenceTest.java
@@ -0,0 +1,25 @@
+package cote.programmers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SumOfContinuedPartialSequenceTest {
+    SumOfContinuedPartialSequence sc = new SumOfContinuedPartialSequence();
+
+    @Test
+    void test1() {
+        assertThat(sc.solution(new int[]{1, 2, 3, 4, 5}, 7)).isEqualTo(new int[]{2, 3});
+    }
+
+    @Test
+    void test2() {
+        assertThat(sc.solution(new int[]{1, 1, 1, 2, 3, 4, 5}, 5)).isEqualTo(new int[]{6, 6});
+    }
+
+    @Test
+    void test3() {
+        assertThat(sc.solution(new int[]{2, 2, 2, 2, 2}, 6)).isEqualTo(new int[]{0, 2});
+    }
+
+}
