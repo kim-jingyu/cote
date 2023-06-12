cc1b2e4071c7489917abb629487103ff3420db3f
jingyu9804 <swc05161@gmail.com>
2023-06-12 21:50:28 +0900

    test(book): 쿼드압축 후 개수 세기

---
+++ src/test/java/book/ch4/quad_compression_counting/QuadCompressionCountingTest.java
@@ -0,0 +1,19 @@
+package book.ch4.quad_compression_counting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class QuadCompressionCountingTest {
+    QuadCompressionCounting qc = new QuadCompressionCounting();
+
+    @Test
+    void test1() {
+        assertThat(qc.solution(new int[][]{{1, 1, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 1}, {1, 1, 1, 1}})).isEqualTo(new int[]{4, 9});
+    }
+
+    @Test
+    void test2() {
+        assertThat(qc.solution(new int[][]{{1, 1, 1, 1, 1, 1, 1, 1}, {0, 1, 1, 1, 1, 1, 1, 1}, {0, 0, 0, 0, 1, 1, 1, 1}, {0, 1, 0, 0, 1, 1, 1, 1}, {0, 0, 0, 0, 0, 0, 1, 1}, {0, 0, 0, 0, 0, 0, 0, 1}, {0, 0, 0, 0, 1, 0, 0, 1}, {0, 0, 0, 0, 1, 1, 1, 1}})).isEqualTo(new int[]{10, 15});
+    }
+}
