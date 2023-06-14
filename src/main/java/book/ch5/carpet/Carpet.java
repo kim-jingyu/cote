8556adc67706af96a3821dc68f27da6e7ebedd1d
jingyu9804 <swc05161@gmail.com>
2023-06-14 21:14:31 +0900

    feat(book): 카펫

---
+++ src/main/java/book/ch5/carpet/Carpet.java
@@ -0,0 +1,18 @@
+package book.ch5.carpet;
+
+class Carpet {
+    public int[] solution(int brown, int yellow) {
+        for (int width = 3; width <= 5000; width++) {
+            for (int height = 3; height <= width; height++) {
+                int boundary = width * 2 + (height - 2) * 2;
+                int center = width * height - boundary;
+
+                if (boundary == brown && center == yellow) {
+                    return new int[]{width, height};
+                }
+            }
+        }
+
+        return null;
+    }
+}
