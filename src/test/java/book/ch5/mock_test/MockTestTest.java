78086e3a28637b3353937998d83470dd16105966
jingyu9804 <swc05161@gmail.com>
2023-06-14 20:37:33 +0900

    test(book): 모의고사

---
+++ src/test/java/book/ch5/mock_test/MockTestTest.java
@@ -0,0 +1,19 @@
+package book.ch5.mock_test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MockTestTest {
+    MockTest mt = new MockTest();
+
+    @Test
+    void test1() {
+        assertThat(mt.solution(new int[]{1, 2, 3, 4, 5})).isEqualTo(new int[]{1});
+    }
+
+    @Test
+    void test2() {
+        assertThat(mt.solution(new int[]{1,3,2,4,2})).isEqualTo(new int[]{1,2,3});
+    }
+}
