acec7402cc9450d4678e0b1ebcec69e5b31a6804
jingyu9804 <swc05161@gmail.com>
2023-06-18 23:11:52 +0900

    feat(book): 모의고사

--- cote-exercise/src/main/java/book/ch6/mock_test/MockTestV1.java
+++ cote-exercise/src/main/java/book/ch6/mock_test/MockTestV1.java
@@ -1,42 +1,38 @@
 package book.ch6.mock_test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MockTestV1 implements MockTest{
     private static final int[][] RULES = {
             {1, 2, 3, 4, 5},
             {2, 1, 2, 3, 2, 4, 2, 5},
-            {3, 3, 1, 1, 2, 2, 4, 4}
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
     };
     public int[] solution(int[] answers) {
-        int[] cnt = new int[RULES.length];
+        List<Integer> result = new ArrayList<>();
+        int[] correct = new int[RULES.length];
+        int max = 0;
+
+        for (int problem = 0; problem < answers.length; problem++) {
+            int answer = answers[problem];
+            for (int person = 0; person < RULES.length; person++) {
+                int index = problem % RULES[person].length;
+                if (answer == RULES[person][index]) {
+                    correct[person]++;
+                    if (correct[person] > max) {
+                        max = correct[person];
+                    }
+                }
+            }
+        }
+
         for (int i = 0; i < RULES.length; i++) {
-            int ruleLength = RULES[i].length;
-            for (int j = 0; j < answers.length; j++) {
-                int curRule = j % ruleLength;
-                if (answers[j] == RULES[i][curRule]) cnt[i]++;
+            if (correct[i] == max) {
+                result.add(i + 1);
             }
         }
 
-        Arrays.sort(cnt);
-
-        for (int i : cnt) {
-            System.out.println("i = " + i);
-        }
-
-        List<Integer> answer = new ArrayList<>();
-        int max = -1;
-        for (int i = RULES.length; i >= 1; i--) {
-            if (cnt[i - 1] >= max) {
-                max = cnt[i - 1];
-                answer.add(4 - i);
-            }
-        }
-
-        return answer.stream()
-                .mapToInt(i -> Integer.parseInt(String.valueOf(i)))
-                .toArray();
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
