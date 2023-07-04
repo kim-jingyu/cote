cb8c9c64e8d8fd88c0b5e97caf0b6d634cf90ac7
JingyuKim <swc05161@gmail.com>
2023-07-04 23:41:50 +0900

    feat: 단어 수학

--- cote-exercise/src/main/java/cote/baekjoon/backtracking/NO1339.java
+++ cote-exercise/src/main/java/cote/baekjoon/backtracking/NO1339.java
@@ -1,7 +1,33 @@
 package cote.baekjoon.backtracking;
 
-public class NO1339 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
+public class NO1339 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[26];
+        for (int i = 0; i < n; i++) {
+            String temp = br.readLine();
+            for (int j = 0; j < temp.length(); j++) {
+                char c = temp.charAt(j);
+                arr[c - 'A'] += (int) Math.pow(10, temp.length() - 1 - j);
+            }
+        }
+
+        Arrays.sort(arr);
+
+        int num = 9;
+        int answer = 0;
+        for (int i = 25; i >= 0; i--) {
+            answer += arr[i] * num;
+            num--;
+        }
+
+        System.out.println(answer);
     }
 }
