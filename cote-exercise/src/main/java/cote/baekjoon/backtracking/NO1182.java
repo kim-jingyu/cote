44cb9b5cd4ce6bb19c0be2ed3306b629cdf163ce
JingyuKim <swc05161@gmail.com>
2023-07-05 22:37:27 +0900

    feat: 부분 수열의 합

--- cote-exercise/src/main/java/cote/baekjoon/backtracking/NO1182.java
+++ cote-exercise/src/main/java/cote/baekjoon/backtracking/NO1182.java
@@ -3,41 +3,37 @@ package cote.baekjoon.backtracking;
 import java.util.Scanner;
 
 public class NO1182 {
-    private static int n,s,answer=0,tmp;
-    private static int[] arr,ch;
-
-    public static void solution(int Level,int sum){
-        if(Level==n){
-            tmp = 0;
-            for(int i:ch){
-                if(i==1) tmp++;
-            }
-            if(sum==s && tmp>0){
-                answer++;
-            }
-        }
-        else{
-            ch[Level] = 1;
-            solution(Level+1,sum + arr[Level]);
-            ch[Level] = 0;
-            solution(Level+1,sum);
-        }
-    }
-
+    private static int n, s, answer = 0;
+    private static int[] arr;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         s = sc.nextInt();
-        arr = new int[n];
-        ch = new int[n];
 
-        for(int i=0;i<n;i++){
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        solution(0,0);
+        calculate(0, 0);
 
-        System.out.println(answer);
+        if (s == 0) {
+            System.out.println(answer - 1);
+        } else {
+            System.out.println(answer);
+        }
+    }
+
+    private static void calculate(int level, int sum) {
+        if (level == n) {
+            if (sum == s) {
+                answer++;
+            }
+            return;
+        }
+
+        calculate(level + 1, sum + arr[level]);
+        calculate(level + 1, sum);
     }
 }
 
