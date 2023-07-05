4ba7d8ad95bcbe8cc5578587324745b2d35dc7ab
JingyuKim <swc05161@gmail.com>
2023-07-05 17:45:25 +0900

    feat: 스타트와 링크

--- cote-exercise/src/main/java/cote/baekjoon/backtracking/NO14889.java
+++ cote-exercise/src/main/java/cote/baekjoon/backtracking/NO14889.java
@@ -2,66 +2,57 @@ package cote.baekjoon.backtracking;
 
 import java.util.Scanner;
 
-public class NO14889 {
-    private static int n,start,link,answer=Integer.MAX_VALUE;
-    private static boolean[] ch;
-    private static int[][] arr;
-
-    public static void solution(int Level,int st){
-        if(Level == n/2){
-            start = 0;
-            link = 0;
-            for(int i=0;i<n;i++){
-                for(int j=i;j<n;j++){
-                    if(ch[i] && ch[j]){
-                        start += arr[i][j];
-                        start += arr[j][i];
-                    } else if (!ch[i] && !ch[j]) {
-                        link += arr[i][j];
-                        link += arr[j][i];
-                    }
-                }
-            }
-            answer = Math.min(answer,Math.abs(start-link));
-        }
-        else{
-            for(int i=st;i<n;i++){
-                if(!ch[i]){
-                    ch[i] = true;
-                    solution(Level+1,i+1);
-                    ch[i] = false;
-                }
-            }
-        }
-    }
-
-
+class NO14889 {
+    private static int n;
+    private static int[][] map;
+    private static boolean[] isVisited;
+    private static int answer = Integer.MAX_VALUE;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        ch = new boolean[n];
+        isVisited = new boolean[n];
 
-        arr = new int[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                arr[i][j] = sc.nextInt();
+        map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i][j] = sc.nextInt();
             }
         }
 
-        solution(0,0);
+        calculate(0, 0);
 
         System.out.println(answer);
     }
+
+    private static void calculate(int level, int current) {
+        if (level == n / 2) {
+            getAnswer();
+            return;
+        }
+
+        for (int i = current; i < n; i++) {
+            if (!isVisited[i]) {
+                isVisited[i] = true;
+                calculate(level + 1, i + 1);
+                isVisited[i] = false;
+            }
+        }
+    }
+
+    private static void getAnswer() {
+        int stark = 0;
+        int link = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i; j < n; j++) {
+                if (isVisited[i] && isVisited[j]) {
+                    stark += map[i][j];
+                    stark += map[j][i];
+                } else if (!isVisited[i] && !isVisited[j]) {
+                    link += map[i][j];
+                    link += map[j][i];
+                }
+            }
+        }
+        answer = Math.min(answer, Math.abs(stark - link));
+    }
 }
-
-//8
-//        0 5 4 5 4 5 4 5
-//        4 0 5 1 2 3 4 5
-//        9 8 0 1 2 3 1 2
-//        9 9 9 0 9 9 9 9
-//        1 1 1 1 0 1 1 1
-//        8 7 6 5 4 0 3 2
-//        9 1 9 1 9 1 0 9
-//        6 5 4 3 2 1 9 0
-
-//1
