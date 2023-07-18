847541d11b932651328a9ede5edfdd9806558d48
JingyuKim <swc05161@gmail.com>
2023-07-19 01:17:51 +0900

    feat: 다음 순열

---
+++ cote-py/baekjoon/step2/NO10972.py
@@ -0,0 +1,22 @@
+import sys
+
+n = int(input())
+arr = list(map(int, sys.stdin.readline().split()))
+target = 0
+canContinue = True
+
+for i in range(n-1, 0, -1):
+    if arr[i-1] < arr[i]:
+        target = i-1
+        break
+else:
+    print(-1)
+    canContinue = False
+
+if canContinue:
+    for i in range(n-1, target, -1):
+        if arr[target] < arr[i]:
+            arr[target], arr[i] = arr[i], arr[target]
+            break
+    arr = arr[:target + 1] + sorted(arr[target + 1:])
+    print(*arr)
