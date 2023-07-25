80989102536b5bca32b74c2410ae63ea67e5f1a6
JingyuKim <swc05161@gmail.com>
2023-07-25 15:48:02 +0900

    feat: 피보나치 수열

--- cote-py/baekjoon/dynamic_programming/NO2748.py
+++ cote-py/baekjoon/dynamic_programming/NO2748.py
@@ -1,15 +1,29 @@
 N = int(input())
 
-cache = [-1] * 91
-cache[0] = 0
-cache[1] = 1
+# cache = [-1] * 91
+# cache[0] = 0
+# cache[1] = 1
+#
+#
+# def fibo(num):
+#     if cache[num] == -1:
+#         cache[num] = fibo(num - 1) + fibo(num - 2)
+#
+#     return cache[num]
+#
+#
+# print(fibo(N))
 
+MOD = 10007
 
-def fibo(num):
-    if cache[num] == -1:
-        cache[num] = fibo(num - 1) + fibo(num - 2)
+cached = [-1] * 91
+cached[0] = 0
+cached[1] = 1
 
-    return cache[num]
+for i in range(2, N + 1):
+    if cached[i] < 0:
+        cached[i] = cached[i-1] + cached[i-2]
+        cached[i] %= MOD
+    print(f'cached[i] = {cached[i]}')
 
-
-print(fibo(N))
+print(cached[N])
