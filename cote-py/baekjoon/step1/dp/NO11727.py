704f7af83544aba9f9b14b4902a072b593319d6e
kim-jingyu <swc05161@gmail.com>
2023-07-28 16:12:13 +0900

    feat: 2Xn 타일링2

---
+++ cote-py/baekjoon/step1/dp/NO11727.py
@@ -0,0 +1,12 @@
+# f(n) = f(n-1) + f(n-2) * 2
+MOD = 10007
+n = int(input())
+cached = [0] * 1001
+cached[1] = 1
+cached[2] = 3
+
+for i in range(3, n+1):
+    cached[i] = cached[i-1] + cached[i-2] * 2
+    cached[i] %= MOD
+
+print(cached[n])
