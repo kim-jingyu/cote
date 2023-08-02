6e7be68f8c309204c80e46ef602ab57e4596e7c7
kim-jingyu <swc05161@gmail.com>
2023-08-02 17:42:50 +0900

    feat: 최소공배수

---
+++ cote-py/baekjoon/step1/math/NO1934.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
+
+
+for _ in range(N):
+    a, b = map(int, input().split())
+    print(lcm(a, b))
