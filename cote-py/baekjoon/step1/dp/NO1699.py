fe0e306f3de4a0b05845c12fa7c29a9c63e6cb92
kim-jingyu <swc05161@gmail.com>
2023-08-25 15:35:36 +0900

    feat: 제곱수의 합 - 성공

--- cote-py/baekjoon/step1/dp/NO1699.py
+++ cote-py/baekjoon/step1/dp/NO1699.py
@@ -9,22 +9,21 @@
 # 9 -> 3^2
 # 10 -> d[1] + 3^2
 # 11 -> d[2] + 3^2
-# 12 -> d[3] + 3^2
+# 12 -> d[3] + 3^2, d[4] + d[8]
 # 13 -> d[4] + 3^2
 # 14 -> d[5] + 3^2
 # 15 -> d[6] + 3^2
 # 16 -> 4^2
-import math
 N = int(input())
-d = [0] * (N + 1)
-temp = 0
+d = [i for i in range(N + 1)]
 
 for i in range(1, N + 1):
-    if math.sqrt(i).is_integer():
-        d[i] = 1
-        temp = 1
-        continue
-    d[i] = d[temp] + 1
-    temp += 1
+    for j in range(i):
+        square = j * j
+        if square > i:
+            break
+
+        if d[i] > d[i - square] + 1:
+            d[i] = d[i - square] + 1
 
 print(d[N])
