eae2edcd7a6991f3eda83e88db991e6f15bcd71c
kim-jingyu <swc05161@gmail.com>
2023-09-04 23:38:07 +0900

    feat: 연속합 2

--- cote-py/baekjoon/step1/dp/NO13398.py
+++ cote-py/baekjoon/step1/dp/NO13398.py
@@ -1,4 +1,13 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-print(arr)
+d = [[0] * n for _ in range(2)]
+
+d[0][0] = arr[0]
+d[1][0] = arr[0]
+
+for i in range(1, n):
+    d[0][i] = max(arr[i], d[0][i-1] + arr[i])
+    d[1][i] = max(d[0][i-1], d[1][i-1] + arr[i])
+
+print(max(max(d[0]), max(d[1])))
