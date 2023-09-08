f7c72de1bd85ceef8a49006e5fe28948c41e4128
kim-jingyu <swc05161@gmail.com>
2023-09-08 22:21:50 +0900

    feat: KiwiJuiceEasy - v2

--- cote-exercise/src/main/java/cote/topcoder/KiwiJuiceEasy.java
+++ cote-exercise/src/main/java/cote/topcoder/KiwiJuiceEasy.java
@@ -6,17 +6,10 @@ public class KiwiJuiceEasy {
             int f = fromId[i];
             int t = toId[i];
 
-            int restSpace = capacities[t] - bottles[t];
+            int amountOfMovement = Math.min(bottles[f], capacities[t] - bottles[t]);
 
-            if (restSpace >= bottles[f]) {
-                int amountOfMovement = bottles[f];
-                bottles[t] += amountOfMovement;
-                bottles[f] -= amountOfMovement;
-            } else {
-                int amountOfMovement = restSpace;
-                bottles[t] += amountOfMovement;
-                bottles[f] -= amountOfMovement;
-            }
+            bottles[t] += amountOfMovement;
+            bottles[f] -= amountOfMovement;
         }
         return bottles;
     }
