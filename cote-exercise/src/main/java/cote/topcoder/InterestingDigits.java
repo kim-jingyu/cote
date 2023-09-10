a9ee0ef7dafae2493ecc2739a2097ca53a8d05f5
kim-jingyu <swc05161@gmail.com>
2023-09-10 20:53:28 +0900

    refactor: InterestingDigits - v2

--- cote-exercise/src/main/java/cote/topcoder/InterestingDigits.java
+++ cote-exercise/src/main/java/cote/topcoder/InterestingDigits.java
@@ -8,20 +8,9 @@ public class InterestingDigits {
         List<Integer> answer = new ArrayList<>();
 
         for (int i = 2; i < base; i++) {
-            boolean ok = true;
-            for (int k1 = 0; k1 < base; k1++) {
-                for (int k2 = 0; k2 < base; k2++) {
-                    for (int k3 = 0; k3 < base; k3++) {
-                        if ((k1 + k2 * base + k3 * base * base) % i == 0 && (k1 + k2 + k3) % i != 0) {
-                            ok = false;
-                            break;
-                        }
-                    }
-                    if (!ok) break;
-                }
-                if (!ok) break;
+            if ((base - 1) % i == 0) {
+                answer.add(i);
             }
-            if (ok) answer.add(i);
         }
 
         return answer.stream().mapToInt(Integer::intValue).toArray();
